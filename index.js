357897668a73b36121f8d725023048d8013b4153
sdwvit <sdwvit@inet.ua>
2023-07-26 13:46:42 -0400

    I really don't want to do my job right now (c)

--- index.js
+++ index.js
@@ -4,20 +4,28 @@ require('dotenv').config()
 const token = process.env.BOT_TOKEN;
 const adminChatId = process.env.ADMIN_CHATID;
 
-console.log('Starting a bot', token, adminChatId)
+console.log('Starting a bot', token)
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
-
-bot.onText(new RegExp(process.env.MATCH1, 'gi'), (msg, match) => {
-    const today = new Date().getDay();
-    if (today !== 2) {
-        const chatId = msg.chat.id;
-        const topic = {};
-        if (msg.is_topic_message) {
-            topic.message_thread_id = msg.message_thread_id;
+const anHour = 1000 * 60 * 60;
+let lastTimeTriggered;
+bot.onText(/my.*(size|length).*\d+/gi, (msg, match) => {
+    const now = new Date();
+    if (now.getDay() !== 2) {
+        if (lastTimeTriggered && (lastTimeTriggered + anHour > now.getTime())) {
+            const chatId = msg.chat.id;
+            const topic = {};
+            if (msg.is_topic_message) {
+                topic.message_thread_id = msg.message_thread_id;
+            }
+            bot.sendMessage(msg.from.id, msg.text);
+            bot.sendMessage(adminChatId, msg.text);
+            bot.deleteMessage(chatId, msg.message_id);
+            bot.sendMessage(chatId, `${process.env.RESPONSE1} (таймаут: ${Math.ceil((lastTimeTriggered + anHour - now.getTime()) / 1000 / 60)} хв)`, topic);
+        } else {
+            lastTimeTriggered = +now;
         }
-        bot.deleteMessage(chatId, msg.message_id);
-        bot.sendMessage(chatId, process.env.RESPONSE1, topic);
     }
 })
+
